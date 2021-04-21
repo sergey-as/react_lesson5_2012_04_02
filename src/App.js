892c747a296b80da3071dc4ef59d0ef8a49fcedf
sergey-as <sergey_as@ukr.net>
2021-04-21 21:07:25 +0300

    12345!

--- src/App.js
+++ src/App.js
@@ -1,54 +1,124 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useParams, useRouteMatch, useLocation, useHistory
 } from "react-router-dom";
-import Posts from "./components/Posts/Posts";
 
 export default function App() {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/posts">Posts</Link>
-                        </li>
-                    </ul>
-                </nav>
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/posts">Posts</Link>
+            </li>
+          </ul>
+        </nav>
 
-                {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-                <Switch>
-                    <Route path="/" component={Home} exact/>
+        <Switch>
+          <Route path="/" component={Home} exact/>
 
-                    <Route path="/posts">
-                        {Posts}
-                    </Route>
+          {/*<Route path="/posts" exact>*/}
+          <Route path="/posts">
+            <Posts/>
+          </Route>
 
-                    <Route>
-                        <h1>PAGE NOT FOUND</h1>
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+          {/*<Route path="/posts/:id">*/}
+          {/*  <PostsDetails/>*/}
+          {/*</Route>*/}
+
+          <Route>
+            <h1>PAGE NOT FOUND</h1>
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 function Home(props) {
-    console.log(props);
-    return <h2>Home</h2>;
+  // console.log(props);
+  return <h2>Home</h2>;
+}
+
+function Posts(props) {
+  // console.log(props);
+  const [posts, setPosts] = useState([]);
+
+  const fetchData = async () => {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const json = await resp.json();
+
+    setPosts(json)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (<div>
+    <Switch>
+      <Route path="/posts/:id" exact>
+        <PostsDetails />
+      </Route>
+
+      <Route>
+        <Redirect to="/posts"/>
+      </Route>
+    </Switch>
+
+    <ul>
+      {posts.map(el => <Link to={`/posts/${el.id}`}><li>{el.title} - {el.id}</li></Link>)}
+    </ul>
+  </div>);
+}
+
+function PostsDetails(props) {
+  // console.log(props);
+  const [post, setPost] = useState();
+  // console.log(props);
+
+  // const params = useParams();
+  const {id} = useParams();
+  const match = useRouteMatch();
+  const location = useLocation();
+  const history = useHistory();
+  // console.log({match, params, location});
+
+
+  const fetchData = async () => {
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const json = await resp.json();
+
+    setPost(json)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [id])
+
+  return (<div>
+    <h1>post details</h1>
+    {post && (<> <h3>{post.id} - {post.title}</h3> <p>{post.body}</p> </>)}
+
+    <button onClick={() => history.push(`/posts/${+id + 1}`)}>go to next post</button>
+  </div>);
 }
 
 
-// export default function App() {
+// export default function App()
+// {
 //     return (
 //         <Router>
 //             <div>
@@ -134,4 +204,4 @@ function Home(props) {
 // }
 
 //https://www.youtube.com/watch?v=oaAVZZypdRU&list=PLY1sAemBLA5xCCp5XcNlPkoyPks72Q7b0&index=57
-// 48:50
+// 53:10
